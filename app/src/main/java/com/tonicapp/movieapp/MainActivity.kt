ed547d2e42d6f905cce3dec147648effb2c16a0b
alunparanggi <alun.paranggi@gmail.com>
2021-09-24 14:40:01 +0700

    finishing bottom navigation bar

--- app/src/main/java/com/tonicapp/movieapp/MainActivity.kt
+++ app/src/main/java/com/tonicapp/movieapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.tonicapp.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.tonicapp.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavBar()
+    }
+
+    private fun setupBottomNavBar() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        navController = navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.menu_movie, R.id.menu_favorite
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
